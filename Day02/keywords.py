65bc39b544c0a28c8403873083ac7030252c205a
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-05 15:28:47 +0530

    keywords

---
+++ Day02/keywords.py
@@ -0,0 +1,7 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import keyword
+>>> print(keyword.kwlist)#list of all keywords
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> print(len(keyword.kwlist))#Total number of keywords
+35
