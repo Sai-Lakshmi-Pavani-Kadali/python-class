bfd65231506467e1fb7c9916f45a5812fcb457fb
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-05 15:43:56 +0530

    condition

---
+++ Day12/ifelse.py
@@ -0,0 +1,16 @@
+
+username=input("username: ")
+password=input("password: ")
+
+if username =='admin' and password =='admin123':
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
+
+'''
+bill = int(input("Enter the bill:"))
+if bill>99:
+    print(bill)
+else:
+    print(bill+30)
+    '''
