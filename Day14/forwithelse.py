efaf62d1d3081c00413f25592217abadf1ed9fcd
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-05 15:48:15 +0530

    for and while loop

---
+++ Day14/forwithelse.py
@@ -0,0 +1,41 @@
+
+
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+else:
+    print("End of the loop")
+
+    '''
+    pin = 1234
+    for_in range(5):
+        epin == int(input("Enter the pin"))
+        if pin=epin:
+            print()
+        
+        
+        ...
+        n = int(input("Enter the number:"))
+        c=0
+        for i inrange(1,n+1)
+        if c==2:
+            print("prime number")
+        else:
+            print("not prime number")
+        
+        n = int(input("Enter the number:"))
+        print("Factors:",end='')
+        for i in range(1,n+1):
+            if n%i==0:
+                print(i,end='')
+        
+        ...
+        n = int(input("Enter the number:"))
+        for i in range(2,n/2+1):
+            if n%i==0:
+                print("Not prime number")
+                break
+        else:
+            print("prime number")
+            '''
