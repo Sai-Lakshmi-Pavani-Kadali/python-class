644e1e18ebc13e34320655d32f2bfe5be587fc33
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-05 15:51:06 +0530

    assert

---
+++ Day15/assert_pass.py
@@ -0,0 +1,25 @@
+'''
+for i in range(10):
+    for j in range(5):
+        print('*',end = ' ')
+    print()
+    '''
+'''   
+for row in range(5):
+    for col in range(5):
+        print('x',end='')
+    print()
+    '''
+'''        
+for row in range(5):
+    for col in range(5):
+        print(col,end=' ')
+    print()
+    '''
+
+'''
+for i in range(5):
+    for j in range(5):
+        print(i+j,end='')
+    print()
+    '''
