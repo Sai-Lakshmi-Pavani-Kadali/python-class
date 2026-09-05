9367b0fd44eb8584e325d1c396fb6677b8d5eada
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-05 15:05:07 +0530

    Introduction

---
+++ Day01/keyword.py
@@ -0,0 +1,12 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import keywords
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import keywords
+ModuleNotFoundError: No module named 'keywords'
+>>> import keyword
+>>> print(keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> print(len(keyword.kwlist))
+35
