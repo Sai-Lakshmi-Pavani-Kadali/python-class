1ffb584d4eb03f0322f2a59f32627cfb6b22befa
Sai-Lakshmi-Pavani-Kadali <sailakshmipavanikadali@gmail.com>
2026-09-07 23:35:54 +0530

    date and time

---
+++ Day26/fileoperations.py
@@ -0,0 +1,37 @@
+'''
+file=open('pfs-63.txt','r')
+print(file.read())
+file.seek(0)
+print(file.readline())
+file.seek(0)
+print(file.readlines())
+file.close()
+'''
+'''
+with open('pfs-63.txt','r') as file:
+    print(file.read())
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())
+    file.close()
+'''
+'''
+with open('mysql.txt','w') as file:
+    file.write("DLL,DML,DCL")
+'''
+'''
+with open('pfs-63.txt','w') as file:
+    file.write("shifted to brach-1")
+
+'''
+'''
+with open('pfs-63.txt','a') as file:
+    file.write("only for today")
+
+'''
+with open('pfs-63.txt','a+') as file:
+    file.write("Tom same branch 5")
+    file.seek(0)
+    print(file.read())
+
